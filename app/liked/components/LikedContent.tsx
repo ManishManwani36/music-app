87d83303f16b24e4b9e8d7b2b7a84bf7f37a4b97
Manish Manwani <manishmanwani73@gmail.com>
2023-06-25 14:33:18 +0930

    feat(liked songs page): added a liked songs page that allows user to view their like songs

---
+++ app/liked/components/LikedContent.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import LikeButton from "@/components/LikeButton";
+import MediaItem from "@/components/MediaItem";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface Props {
+  songs: Song[];
+}
+
+export default function LikedContent({ songs }: Props) {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, router, user]);
+
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No liked songs. Like a song to see it here.
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div className="flex items-center gap-x-4 w-full" key={song.id}>
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -9,6 +9,8 @@ interface Props {
   };
 }
 
+export const revalidate = 0;
+
 export default async function Search({ searchParams }: Props) {
   const songs = await getSongsByTitle(searchParams.title);
   return (
